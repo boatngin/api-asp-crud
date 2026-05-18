18dffde81db22ecb1cb7c95e34ec6c06fd28ddcd
boatengine <thanapat.boat16@gmail.com>
2026-05-18 22:16:14 +0700

    EF to db

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using Backapi.Models;
+
+namespace Backapi.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users => Set<User>();
+    }
+}
